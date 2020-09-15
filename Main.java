90ffc8d3baec54438ebfeb1bc7b844c0fd4f0c23
pajid9763 <pajid9763@wrdsb.ca>
2020-09-15 15:43:25 +0000

    final

--- Main.java
+++ Main.java
@@ -17,21 +17,21 @@ public class Main {
   double num = input.nextInt();
 
   // find their discount
-  if (num > 0.01 && num < 40.00){
+  if (num > 0.01 && num <= 40.00){
     System.out.println("They will receive 10% off.");
     double save1 = num * 0.1;
     System.out.println("They will save $" + save1 + ".");
     double total1 = num - save1;
     System.out.println("The new total is $" + total1 + ".");
   
-  }else if (num > 40.00 && num < 80.00){
+  }else if (num > 40.01 && num <= 80.00){
     System.out.println("They will receive 20% off.");
     double save2 = num * 0.2;
     System.out.println("They will save $" + save2 + ".");
     double total2 = num - save2;
     System.out.println("The new total is $" + total2 + ".");
   
-  }else if (num > 80.00 && num < 120.00){
+  }else if (num > 80.01 && num <= 120.00){
     System.out.println("They will receive 30% off.");
     double save3 = num * 0.3;
     System.out.println("They will save $" + save3 + ".");
